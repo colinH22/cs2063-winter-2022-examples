b2e2a0c214abd92c2282e3bea84f0b6ed9119e4d
Howard Powell <hspowell@gmail.com>
2022-02-13 11:50:03 -0400

    Addition of Lecture 6 examples

---
+++ Lecture 6/NetworkingURL/app/src/main/java/mobiledev/unb/ca/networkingurl/RetainedFragment.java
@@ -0,0 +1,47 @@
+package mobiledev.unb.ca.networkingurl;
+
+import android.app.Fragment;
+import android.content.Context;
+import android.os.Bundle;
+
+public class RetainedFragment extends Fragment {
+    static final String TAG = "RetainedFragment";
+    private OnFragmentInteractionListener mListener;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRetainInstance(true);
+    }
+
+    void onButtonPressed() {
+        new HttpGetTask(this).execute();
+    }
+
+    void onDownloadFinished(String result) {
+        if (null != mListener) {
+            mListener.onDownloadFinished(result);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    public interface OnFragmentInteractionListener {
+        void onDownloadFinished(String result);
+    }
+}
